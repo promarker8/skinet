b3b42bfff0284af5fb670b2500b4e945ce78f58d
rebeccaq1712 <rebecca.quinn@ucdconnect.ie>
2023-08-17 00:38:09 +0100

    End of section 3

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,19 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+
+        // related entities (ProductType and ProductBrand)
+        // to help entity famework
+        // when we create a new migration, it's going to know that the product has a relationship with both the product type and the product brand.
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
+
 }
